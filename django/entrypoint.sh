00bf060cf60cdef3443b38d98208df0afc625cc6
Msic <claver.developer@gmail.com>
2020-10-06 02:18:53 +0300

    Initial commit

---
+++ django/entrypoint.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+set -o errexit
+set -o nounset
+
+postgres_ready() {
+python << END
+import sys
+import psycopg2
+
+try:
+    psycopg2.connect("host='${DATABASE_HOST}' \
+                      port='${DATABASE_PORT}' \
+                      dbname='${DATABASE_NAME}' \
+                      user='${DATABASE_USER}' \
+                      password='${DATABASE_PASSWORD}'")
+except psycopg2.OperationalError:
+    sys.exit(-1)
+sys.exit(0)
+END
+}
+until postgres_ready; do
+  >&2 echo 'Waiting for PostgreSQL to become available...'
+  sleep 1
+done
+>&2 echo 'PostgreSQL is available'
+
+exec "$@"
